566cee167b114b059553473d32ccd35a6d06fb3e
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2017-08-13 13:06:39 +0200

    Add update action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
                   path: '/' do # namespace as the folder in app/controllers/api # check available MIME types by typing `$ Mime::SET.collect(&:to_s)
     scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, only: [:show, :create]
+      resources :users, only: [:show, :create, :update]
     end
   end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -52,4 +52,44 @@ RSpec.describe Api::V1::UsersController, type: :controller do # changed to Api::
       it { should respond_with 422 }
     end
   end
+
+  describe "PUT/PATCH #update" do
+    context "when is succesfully updated" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, params: { id: @user.id,
+                                 user: { email: "newmail@example.com" } },
+                                 format: :json
+      end
+
+      it "renders the json representation for the updated user" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql "newmail@example.com"
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "when is not succesfully updated" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, params: { id: @user.id,
+                                 user: { email: "invalidmail.com" } },
+                                 format: :json
+      end
+
+      it "renders the errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the errors json on why the user was not updated" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "is invalid"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+
 end

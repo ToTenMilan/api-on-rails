d0f361d088063e2f1d7938f27b1abda0fc1e874b
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2017-08-15 14:15:11 +0200

    Add authenticate_with_token! method to handle access to actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :auth_token, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :authenticatable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   before_create :generate_authentication_token!
 

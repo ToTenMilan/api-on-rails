adca17d68a401e155213dc8b2a8c1113c38a5d3a
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2017-08-15 08:42:51 +0200

    Add sessions controller

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe User do
   it { should respond_to(:email) } # like @user.email
   it { should respond_to(:password) } # like @user.password
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:auth_token) }
 
   it { should be_valid } # this method saves the @user setting it's id
 
@@ -18,9 +19,27 @@ RSpec.describe User do
 
     # methods added by shoulda-matchers
     it { should validate_presence_of(:email) }
-    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
-    it { should validate_confirmation_of(:password) }
-    it { should allow_value('example@domain.com').for(:email) }
+  end
+
+  it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
+  it { should validate_uniqueness_of(:auth_token) }
+
+
+  describe "#generate_authentication_token!" do
+    it "generates a unique token" do
+      # Devise.stub(:friendly_token).and_return("auniquetoken123") # imitate method friendly_token that returns some token
+      allow(Devise).to receive(:friendly_token).and_return("auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to eql "auniquetoken123"
+    end
+
+    it "generates another token when one already has been taken" do
+      existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).not_to eql existing_user.auth_token
+    end
   end
 
 end

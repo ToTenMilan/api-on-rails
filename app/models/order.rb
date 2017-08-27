0a4f21f612b6f8f7ab6d59b00aae08fae04e330d
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2017-08-27 14:28:42 +0200

    Add the create method for orders controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,9 @@ class Order < ApplicationRecord
   validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
   has_many :placements
   has_many :products, through: :placements
+  before_validation :set_total!
+
+  def set_total!
+    self.total = products.map(&:price).sum
+  end
 end
